a3d27e915313efe231c4eab99022460babb4b203
pdmxdd <pdmxdd@gmail.com>
2021-07-25 21:55:22 -0500

    entity

---
+++ src/main/java/dev/paulmatthews/baseormexample/models/Person.java
@@ -0,0 +1,45 @@
+package dev.paulmatthews.baseormexample.models;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Person {
+    @Id
+    private int id;
+    private String firstName;
+    private String lastName;
+
+    public Person(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public Person() {
+
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
