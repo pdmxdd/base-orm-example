608f797e048cecfb59a3832d5064f499d6705895
pdmxdd <pdmxdd@gmail.com>
2021-07-25 21:46:13 -0500

    base ORM -- just setup

---
+++ src/main/java/dev/paulmatthews/baseormexample/BaseOrmExampleApplication.java
@@ -0,0 +1,13 @@
+package dev.paulmatthews.baseormexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaseOrmExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BaseOrmExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/paulmatthews/baseormexample/BaseOrmExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.paulmatthews.baseormexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaseOrmExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
